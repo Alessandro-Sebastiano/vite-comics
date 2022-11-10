c2eb121265b2a6e6b9da3dc8bef07e1930765de7
Alessandro-Sebastiano <alesebastiano15@gmail.com>
2022-11-10 16:12:24 +0100

    content

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <template>
     <HeaderComponent />
+    <ContentComponent />
 </template>
 
 <script>
 import HeaderComponent from './components/HeaderComponent.vue';
-
+import ContentComponent from './components/ContentComponent.vue';
 
 export default {
     components: {
         HeaderComponent,
+        ContentComponent,
     }
 }
 </script>
---
+++ src/components/ContentComponent.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="content">
+        <div class="component-container">
+            <h2>{{ contenTitle }}</h2>
+        </div>
+    </div>
+</template>
+
+<script>
+
+name: 'ContentComponent';
+
+export default {
+    data() {
+        return {
+            contenTitle: '--> Content goes here <--'
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@use '../assets/styles/partials/_mixins.scss' as *;
+@use '../assets/styles/partials/variables' as*;
+
+
+.content {
+    background-color: $dark;
+    padding: 26px 0px;
+}
+
+.component-container {
+    @include general-container;
+
+    h2 {
+        color: $ligth;
+    }
+}
+</style>
